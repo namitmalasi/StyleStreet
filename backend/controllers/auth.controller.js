40e1e64c46748e5c3a3efdb2519f4242334ff74f
Namit Malasi <namitmalasi@gmail.com>
2024-09-19 20:40:42 +0530

    backend setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.send("Sign up route called");
+};
+
+export const signin = async (req, res) => {
+  res.send("Sign in route called");
+};
+
+export const logout = async (req, res) => {
+  res.send("logout route called");
+};
