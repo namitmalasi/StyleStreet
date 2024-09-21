db96732e4eaec23deff466fbd2de3e10e207f13a
Namit Malasi <namitmalasi@gmail.com>
2024-09-21 13:46:04 +0530

    generating tokens and authenticating user in sign up

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,42 @@
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+import { redis } from "../lib/redis.js";
+
+const generateTokens = (userId) => {
+  const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN, {
+    expiresIn: "15m",
+  });
+  const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN, {
+    expiresIn: "7d",
+  });
+
+  return { accessToken, refreshToken };
+};
+
+const storeRefreshToken = async (userId, refreshToken) => {
+  await redis.set(
+    `refersh_token:${userId}`,
+    refreshToken,
+    "Ex",
+    7 * 24 * 60 * 60
+  ); //7days
+};
+
+const setCookies = (res, accessToken, refreshToken) => {
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true, //prevents xss attacks i.e cross-site scripting
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict", //prevents CSRF attacks i.e.cross-site request forgery
+    maxAge: 15 * 60 * 1000,
+  });
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true, //prevents xss attacks i.e cross-site scripting
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict", //prevents CSRF attacks i.e.cross-site request forgery
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+};
 
 export const signup = async (req, res) => {
   const { name, email, password } = req.body;
@@ -9,7 +47,20 @@ export const signup = async (req, res) => {
 
     const user = await User.create({ name, email, password });
 
-    return res.status(201).json({ user, message: "User created successfully" });
+    // authenticate
+    const { accessToken, refreshToken } = generateTokens(user._id);
+    await storeRefreshToken(user._id, refreshToken);
+    setCookies(res, accessToken, refreshToken);
+
+    return res.status(201).json({
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+      message: "User created successfully",
+    });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
