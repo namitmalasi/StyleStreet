04b30321e6b67c08be87a59572681be25c05a165
Namit Malasi <namitmalasi@gmail.com>
2024-09-23 19:07:10 +0530

    fetured products controller and cloudinary setup

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,3 +1,4 @@
+import { redis } from "../lib/redis.js";
 import Product from "../models/product.model.js";
 
 export const getAllProducts = async (req, res) => {
@@ -10,4 +11,31 @@ export const getAllProducts = async (req, res) => {
       .status(500)
       .json({ message: "Products fetching failed", error: error.message });
   }
+};
+
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    // first search for featured products in redis
+    let featuredProducts = await redis.get("featured_products");
+    if (featuredProducts) return res.json(JSON.parse(featuredProducts));
+
+    // if not present in redis search in mongoDb
+    featuredProducts = await Product.find({ isFeatured: true }).lean();
+
+    if (!featuredProducts)
+      return res.status(404).json({ message: "No featured Products found" });
+
+    await redis.set("featured_products", JSON.stringify(featuredProducts));
+
+    return res.json(featuredProducts);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "server error", error: error.message });
+  }
+};
+
+export const createProduct = (req, res  ) => {
+  try {
+  } catch (error) {}
 };
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { getAllProducts } from "../controllers/product.controller.js";
+import {
+  getAllProducts,
+  getFeaturedProducts,
+  createProduct,
+} from "../controllers/product.controller.js";
 import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
+router.post("/", protectRoute, adminRoute, createProduct);
 
 export default router;
